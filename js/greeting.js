c4adf69de8dc3de16b82a745b596e8c6ddebf42b
axeltrinidade <68217806+axeltrinidade@users.noreply.github.com>
2020-12-04 02:25:49 +0530

    Update greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -3,18 +3,18 @@ var today = new Date();
 var hour = today.getHours();
 
 // Here you can change your name
-var name = 'Axel T';
+var name = 'Axel';
 
 // Here you can change your greetings
 var gree1 = 'Focus! ';
 var gree2 = 'Good morning! ';
-var gree3 = 'Good afternoon ';
-var gree4 = 'Good evening ';
+var gree3 = 'Good afternoon, ';
+var gree4 = 'Where to? ';
 var gree5 = 'ᕕ( ᐛ )ᕗ ';
 var gree6 = 'Good evening ';
 
 // Define the hours of the greetings
-if (hour >= 23 && hour < 5) {
+if (hour >= 1 && hour < 5) {
     document.getElementById('greetings').innerText = gree1 + name;
 } else if (hour >= 6 && hour < 12) {
     document.getElementById('greetings').innerText = gree2 + name;
